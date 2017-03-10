7707575e437e5b5abc51990ef9ef13ed623620b2
Natalia Larrieux <natalia.larrieux@modelit.xyz>
2017-03-10 19:43:20 -0300

    Indentation

--- index.js
+++ index.js
@@ -12,36 +12,39 @@ app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
 app.get('/', function(request, response) {
-  response.render('pages/index');
+    response.render('pages/index');
 });
 
 app.get('/cool', function(request, response) {
-  response.send(cool());
+    response.send(cool());
 });
 
 app.get('/times', function(request, response) {
-    var result = ''
-    var times = process.env.TIMES || 5
-    for (i=0; i < times; i++)
+    var result = '';
+    var times = process.env.TIMES || 5;
+    for (i=0; i < times; i++) {
       result += i + ' ';
-  response.send(result);
+    }
+    response.send(result);
 });
 
 app.get('/db', function (request, response) {
-  pg.connect(process.env.DATABASE_URL, function(err, client, done) {
-    client.query('SELECT * FROM availability', function(err, result) {
-        console.log('result', result);
-      done();
-      if (err)
-       { console.error(err); response.send("Error " + err); }
-      else
-       { response.render('pages/db', {results: result.rows} ); }
+    pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+        client.query('SELECT * FROM availability', function(err, result) {
+            console.log('result', result);
+            done();
+            if (err) { 
+                console.error(err); 
+                response.send("Error " + err); 
+            } else { 
+                response.render('pages/db', {results: result.rows} ); 
+            }
+        });
     });
-  });
 });
 
 app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
+    console.log('Node app is running on port', app.get('port'));
 });
 
 
